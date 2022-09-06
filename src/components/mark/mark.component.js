251e55a7656a2af16b8304f3e0b1404e89b16450
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-09-06 12:51:36 +0530

    source button added

--- src/components/mark/mark.component.js
+++ src/components/mark/mark.component.js
@@ -14,14 +14,26 @@ const Mark = ({mark}) => {
           <CCardTitle>{title}</CCardTitle>
           {
             mark.url
-              ? <Link to={{
-                  pathname: `/marks/${mark.id}`,
-                  state: { markInfo: mark}}}>
-                  <CButton color="dark" size="sm">{mark.buttonText}</CButton>
-                </Link>
-              : <Link to={`${mark.link}`}>
-                  <CButton color="dark" size="sm">{mark.buttonText}</CButton>
-                </Link>
+              ? 
+                <div className="detail-buttons-container">
+                  <Link to={{
+                    pathname: `/marks/${mark.id}`,
+                    state: { markInfo: mark}}}>
+                    <CButton color="dark" size="sm">{mark.buttonText}</CButton>
+                  </Link>
+                  <a href={`${mark.source}`} target="_blank" rel="noopener noreferrer" style={{marginLeft: '10px'}}>
+                    <CButton color="dark" size="sm">View Source</CButton>
+                  </a>
+                </div>
+              : 
+                <div className="detail-buttons-container">
+                  <Link to={`${mark.link}`}>
+                    <CButton color="dark" size="sm">{mark.buttonText}</CButton>
+                  </Link>
+                  <a href={`${mark.source}`} target="_blank" rel="noopener noreferrer" style={{marginLeft: '10px'}}>
+                    <CButton color="dark" size="sm">View Source</CButton>
+                  </a>
+                </div>
           }
         </CCardBody>
       </CCard>
