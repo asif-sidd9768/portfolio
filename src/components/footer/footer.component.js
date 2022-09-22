de3fdc0f79a6dccb23df9057b1f8be9b7feb32b3
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-09-22 18:23:50 +0530

    Changes on footer link

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -3,7 +3,7 @@ import './footer.styles.css'
 const Footer = () => {
   return (
     <div>
-      <p className="footer-text">Designed by @Asif Siddique</p>
+      <p className="footer-text">Designed by @Asif Siddique | <a className="footer-read-more-txt" href="https://github.com/asif-sidd9768/portfolio">Read More</a></p>
     </div>
   )
 }
